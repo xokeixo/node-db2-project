dcc7086698d9856c6b62a6afa22ba13423ae4345
Markeisha <markeisha94@gmail.com>
2022-02-15 13:38:28 -0600

    built out router & model

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,29 @@
 // DO YOUR MAGIC
+const express = require('express');
+const Car = require('./cars-model');
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+    try {
+        const cars = await Car.getAll()
+        res.json(cars)
+    } catch (err) {
+        next(err)
+    }
+});
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const car = await Car.getById(req.params.id)
+        res.json(car)
+    } catch (err) {
+        next(err)
+    }
+});
+
+router.post('/', async (req, res, next) => {
+    res.json('posting new car')
+})
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,15 +1,20 @@
-const express = require("express")
+const express = require("express");
+const carsRouter = require('./cars/cars-router');
+const server = express();
 
-const server = express()
+
 
 // DO YOUR MAGIC
 server.use(express.json());
 
+server.use('/api/cars', carsRouter);
+
 server.use('*', (req, res, next) => {
     next({ status: 404, message: 'not found' })
-})
+});
 
 server.use((err, req, res, next) => {
     res.status(err.status || 500).json({ message: err.message })
-})
-module.exports = server
+});
+
+module.exports = server;
